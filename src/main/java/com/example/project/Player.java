30764d403fbc188720e8edd12532f864efde996b
benjamins266 <benjamins266@nycstudents.net>
2025-02-28 17:34:15 +0000

    Project

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -16,21 +16,115 @@ public class Player{
     public ArrayList<Card> getAllCards(){return allCards;}
 
     public void addCard(Card c){
+        hand.add(c);
+    }
+
+    public String playHand(ArrayList<Card> communityCards){   
+        allCards = new ArrayList<>(hand);
+        for (Card card1 : communityCards) {
+            allCards.add(card1);
+        }
+        sortAllCards();
+        String outcome = "Nothing";
+
+        ArrayList<Integer> rankF = findRankingFrequency();
+        ArrayList<Integer> suitF = findSuitFrequency();
+
+        if (highCard()) {
+            outcome = "High Card";
+        }
+        if(onePair()){
+            outcome = "A Pair";
+        }
+        if(twoPair()){
+            outcome = "Two Pair";
+        }
         
+        return outcome;
+   }
+
+
+    private boolean highCard() {
+        int highestValue = Integer.MIN_VALUE;
+        for (int i = 0; i < hand.size(); i ++) {
+            if (Utility.getRankValue(hand.get(i).getRank()) > highestValue) {
+                highestValue = Utility.getRankValue(hand.get(i).getRank());
+            }
+        }
+        if (Utility.getRankValue(allCards.get(allCards.size() - 1).getRank()) == highestValue) {
+            return true;
+        }
+        return false;
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
-        return "Nothing";
+    private boolean onePair(){
+        ArrayList<Integer> rankFrequency = findRankingFrequency();
+        for(int i = 0; i<rankFrequency.size(); i++){
+            if(rankFrequency.get(i)>1){
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void sortAllCards(){} 
+    private boolean twoPair(){
+        ArrayList<Integer> rankFrequency = findRankingFrequency();
+        int count = 0;
+        for(int i = 0; i<rankFrequency.size(); i++){
+            if(rankFrequency.get(i)>1){
+                count++;
+            }
+        }
+        if(count == 2){
+            return true;
+        }
+        return false;
+
+    }
+
+
+
+    public void sortAllCards(){
+        for(int i = 0; i<allCards.size(); i++){
+            int lowestValue = i;
+            for(int j = i; j<allCards.size(); j++){
+                if(Utility.getRankValue(allCards.get(j).getRank())<Utility.getRankValue(allCards.get(lowestValue).getRank())){
+                    lowestValue = j;
+                }
+            }
+            Card temp = allCards.get(i);
+            allCards.set(i, allCards.get(lowestValue));
+            allCards.set(lowestValue,temp);
+        }   
+        }
+ 
 
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> frequency = new ArrayList<>();
+        for(int i = 0; i<ranks.length; i++){
+            int count = 0;
+            for(int j = 0; j<allCards.size(); j++){
+                if(Utility.getRankValue(allCards.get(j).getRank()) == Utility.getRankValue(ranks[i])){
+                    count++;
+                }
+            }
+            frequency.add(count);
+        }
+        return frequency; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> suitFrequency = new ArrayList<>();
+        for(int i = 0; i<suits.length; i++){
+            int count = 0;
+            for(int j = 0; j<allCards.size(); j++){
+                if(allCards.get(j).getSuit().equals(suits[i])){
+                    count++;
+                }
+            }
+            suitFrequency.add(count);
+        }
+        return suitFrequency; 
     }
 
    
@@ -38,8 +132,5 @@ public class Player{
     public String toString(){
         return hand.toString();
     }
-
-
-
 
 }
